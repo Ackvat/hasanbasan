3efd11d82fb582e74c600d1894e9916f83ec0af7
Ackvat <toprak_karakaya@hotmail.com>
2025-02-13 04:49:35 +0300

    step motor test

--- stepmotortest.py
+++ stepmotortest.py
@@ -1,17 +1,27 @@
 from machine import Pin
 from time import sleep
 
-IN1 = Pin(2,Pin.OUT)
-IN2 = Pin(3,Pin.OUT)
-IN3 = Pin(4,Pin.OUT)
-IN4 = Pin(5,Pin.OUT)
+IN1 = Pin(10,Pin.OUT)
+IN2 = Pin(11,Pin.OUT)
+IN3 = Pin(12,Pin.OUT)
+IN4 = Pin(13,Pin.OUT)
 
 pins = [IN1, IN2, IN3, IN4]
 
 sequence = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
 
+print("Step testi baslatiliyor...")
+
 while True:
-    for step in sequence:
-        for i in range(len(pins)):
-            pins[i].value(step[i])
-            sleep(0.1)
+    try:
+        for step in sequence:
+            for i in range(len(pins)):
+                pins[i].value(step[i])
+            sleep(0.002)
+    except KeyboardInterrupt:
+        break
+
+for i in range(len(pins)):
+    pins[i].value(0)
+
+print("Step testi durduruldu!")
